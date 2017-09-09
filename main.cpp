ebd13a0af3f7437b3c480b7b4014017c94d3bd35
shicky256 <shicky256@gmail.com>
2017-09-09 14:21:53 -0400

    made the program into more of a launcher

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
 	DWORD memAddrs[] = {
     	0x00838768,
-    	0x00906158,
+   	0x00906158,
     	0x009060D8,
     	0x008FFA58,
     	0x008CE7B4,
@@ -15,11 +15,19 @@ int main() {
 	};
 	DWORD width=320; //change to the width you want Mania to render to (default is 424)
 	DWORD processID;
-	HWND Window=FindWindow(NULL,"Sonic Mania");
-	if (!GetWindowThreadProcessId(Window,&processID)) {
-		cerr << "couldn't find the Sonic Mania process, " << GetLastError() << endl;
+	HWND window;
+	if (!WinExec("SonicMania.exe",SW_SHOWNORMAL)) {
+		cerr << "couldn't find the Sonic Mania executable, make sure this program is in your Sonic Mania folder" << GetLastError << endl;
 		return 1;
 	}
+	else 
+		cout << "waiting for Sonic Mania to start";
+	while (!GetWindowThreadProcessId(window,&processID)) {
+		cout << ".";
+		window=FindWindow(NULL,"Sonic Mania");
+		Sleep(250);
+	}
+	cout << endl;
 	HANDLE handle=OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
 	if (!handle) {
 		cerr << "couldn't open the Sonic Mania process, " << GetLastError() << endl;
@@ -27,6 +35,8 @@ int main() {
 	}
 	else 
 		cout << "Sonic Mania process found, id "<< processID << endl;
+	//cout << "waiting for Sonic Mania to initialize..." <<endl;
+	//Sleep(5000);
 	cout << "patching..." << endl;
 	int i;
 	for (i=0; i < 9; i++) {
@@ -43,9 +53,16 @@ int main() {
 		cout << ", new value: " << memory << endl;
 		memory=0;
 	}
-	cout << "done patching, exiting..." << endl;
+	cout << "done patching" << endl;
+
+	cout << "making Mania fullscreen..." << endl;
+	keybd_event(VK_MENU, 0, 0, 0);
+	keybd_event(VK_RETURN, 0, 0, 0);
+	keybd_event(VK_RETURN,0,KEYEVENTF_KEYUP,0);
+	keybd_event(VK_MENU,0,KEYEVENTF_KEYUP,0);
 	
-	CloseHandle(Window);
+	CloseHandle(window);
 	CloseHandle(handle);
+	//Sleep(20000);
 	return 0;
 }
